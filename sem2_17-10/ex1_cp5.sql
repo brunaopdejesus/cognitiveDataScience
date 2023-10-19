e172844f0f0bceb25c3893f666b68f8c4871023d
Bruna Oliveira <bruna.opdejesus@gmail.com>
2023-10-19 11:21:37 -0300

    att

--- sem2_17-10/ex1_cp5.sql
+++ sem2_17-10/ex1_cp5.sql
@@ -1,22 +1,8 @@
--- Criar as chaves primárias e estrangeiras.
-
--- As colunas PRODUTO e MATERIA PRIMA devem ser únicas.
--- Nome das Restrições
-    -- Coluna PRODUTO: PRODUTO UK,
-    -- Coluna MATERIA PRIMA : MATERIA PRIMA UK
-
--- A coluna QUANTIDADE não pode aceitar NULO.
-
--- A coluna MATERIA PRIMA deve permitir a inserção dos seguintes dados: MADEIRA, PAPELÃO ou INOX. 
--- Nomeie essa restrição de: 
-    -- MATERIA PRIMA CK
-
--- Nomeie as restrições PK e FK conforme as informações do diagrama.
-
 CREATE TABLE materia_prima (
     cod_materia_prima INTEGER CONSTRAINT materia_prima_pk PRIMARY KEY,
     materia_prima VARCHAR(255)
 );
+
 
 CREATE TABLE produto (
     cod_produto INTEGER CONSTRAINT produto_pk PRIMARY KEY,
@@ -31,12 +17,29 @@ CREATE TABLE item_produto(
     CONSTRAINT item_produto_pk PRIMARY KEY (cod_produto, cod_materia_prima)
 );
 
-desc materia_prima;
-desc produto;
-desc item_produto;
-
 ALTER TABLE item_produto
 MODIFY quantidade NOT NULL;
 
+ALTER TABLE produto
+    ADD CONSTRAINT item_produto_fk FOREIGN KEY (cod_produto)
+        REFERENCES item_produto (cod_produto);
+
+ALTER TABLE materia_prima
+    ADD CONSTRAINT item_produto_fk FOREIGN KEY (cod_item_produto)
+        REFERENCES item_produto (cod_produto);
+
 ALTER TABLE materia_prima
 MODIFY materia_prima VARCHAR2(50) CONSTRAINT materia_prima_ck CHECK (materia_prima IN ('MADEIRA', 'PAPELÃO', 'INOX'));
+
+
+
+
+
+
+
+
+
+
+
+
+
